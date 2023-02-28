7c179e22c85f7a06fdd20206c9599176dd08d52e
EC2 Default User <117144349+ryo59124@users.noreply.github.com>
2023-02-28 11:44:57 +0000

    event

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-(function(){
-  ('.box1').css({
-    'background-color': '#0000FF',
-    'height': '100px'
+$(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color': '#0000FF'});
   });
 });
